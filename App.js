5b09a72d6c4b66b558ff263e08eef78d887f1006
타래 <bae94157617@gmail.com>
2021-11-16 23:26:55 +0900

    1116

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityYourPos}>You positioned..</Text>
+        <Text style={styles.cityName}>Daegu</Text>
+      </View>
+      <View style={styles.weather}>
+        <Text style={styles.dayTemp}>17℃</Text>
+        <Text style={styles.dayClimate}>sunny</Text>
+      </View>
     </View>
   );
 }
 
+// style={styles.}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#C5C5E6",
+  },
+  city: {
+    flex: 1,
+    // backgroundColor: "#f8a032",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 60,
+    margin: 10,
+    fontWeight: "600",
+    color: "white",
+  },
+  cityYourPos: {
+    fontSize: 15,
+    margin: 5,
+    color: "white",
+  },
+  weather: {
+    flex: 2.5,
+    alignItems: "center",
+  },
+  dayTemp: {
+    fontSize: 138,
+    fontWeight: "500",
+    marginTop: 50,
+    color: "white",
+  },
+  dayClimate: {
+    marginTop: -10,
+    fontSize: 25,
+    color: "white",
   },
 });
